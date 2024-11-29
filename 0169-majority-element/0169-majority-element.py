5686bb8edabe00af4e86e91a6ec8a9ace68d5b3f
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2024-11-30 00:13:14 +0300

    Time: 3 ms (82.71%), Space: 18.8 MB (6.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,19 +1,8 @@
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter1 = 0
-        counter2 = 0
-
-        num1 = 0
-        num2 = 0
-
-        for i in nums:
-            while i in nums:
-                counter1 += 1
-                num1 = i
-                nums.remove(i)
-            if counter1 > counter2:
-                counter2 = counter1
-                counter1 = 0
-                num2 = num1
-        return num2
-            
+        # Sort the array
+        nums.sort()
+        # The majority element will always be at the middle for a sorted array
+        return nums[len(nums) // 2]
+        
