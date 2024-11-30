499c437ad38c83c882c7aebd6f7c3c0d17605955
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2024-11-30 23:32:41 +0300

    Time: 3 ms (14.54%), Space: 17.1 MB (5.19%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,31 +3,4 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Selection Sort
-
-        for i in range(len(nums)):
-            minn = nums[i]
-            indexx = i
-            for j in range(i+1, len(nums)):
-                if nums[j] <= minn:
-                    minn = nums[j]
-                    indexx = j
-            nums[indexx] = nums[i]
-            nums[i] = minn
-
-        # #  Bubble sort
-
-        for i in range(len(nums)):
-            for j in range(len(nums)-1):
-                if nums[j] > nums[j+1]:
-                    nums[j], nums[j+1] = nums[j+1], nums[j]
-
-        # insertion sort
-
-        for i in range(1, len(nums)):
-            key = nums[i]
-            j = i - 1
-            while j >= 0 and nums[j] > key:
-                nums[j + 1] = nums[j]
-                j -= 1
-            nums[j + 1] = key
+        nums.sort()
