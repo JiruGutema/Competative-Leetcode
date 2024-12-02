66a8e9b2b47d4b43c374bfd765f39280eeecb74c
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2024-12-02 22:52:37 +0300

    Time: 0 ms (100%), Space: 17.4 MB (5.2%) - LeetHub

--- 1009-pancake-sorting/1009-pancake-sorting.py
+++ 1009-pancake-sorting/1009-pancake-sorting.py
@@ -2,32 +2,14 @@ from typing import List
 
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        ans = []
         
-        def flip(k):
-            start = 0
-            while start < k:
-                arr[start], arr[k] = arr[k], arr[start]
-                start += 1
-                k -= 1
-
-        n = len(arr)
-        for i in range(n - 1, -1, -1):
-            # Find the index of the maximum element in arr[0..i]
-            max_index = 0
-            for j in range(1, i + 1):
-                if arr[j] > arr[max_index]:
-                    max_index = j
-            
-            # If the maximum element is not already in the correct place
-            if max_index != i:
-                # Flip the maximum number to the front if it's not already there
+        res = []
+        for i in range(len(arr), 1, -1):
+            max_index = arr.index(max(arr[:i]))
+            if max_index != i - 1:
                 if max_index != 0:
-                    flip(max_index)
-                    ans.append(max_index + 1)
-                
-                # Flip it to its correct position
-                flip(i)
-                ans.append(i + 1)
-
-        return ans
+                    arr[:max_index+1] = arr[:max_index+1][::-1]
+                    res.append(max_index + 1)
+                arr[:i] = arr[:i][::-1]
+                res.append(i)
+        return res
