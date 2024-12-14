8996d5b325326ef8fdacadb9a82393969560fabb
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2024-12-14 20:18:57 +0300

    Time: 253 ms (20.73%), Space: 17.9 MB (17.07%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,29 +1,28 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        k = len(p)
-        p = sorted(p)
-        start = 0
-        end = k
-        ans = []
+        # k = len(p)
+        # p = sorted(p)
+        # start = 0
+        # end = k
+        # ans = []
 
-        while end < len(s)+1:
-            if p == sorted(s[start:end]):
-                ans.append(start)
-            start += 1
-            end += 1
-        return ans
+        # while end < len(s)+1:
+        #     if p == sorted(s[start:end]):
+        #         ans.append(start)
+        #     start += 1
+        #     end += 1
+        # return ans
+        
+        result = []
+        target = collections.Counter(p)
+        window = collections.Counter(s[:len(p) - 1])
+        for i in range(len(p) - 1, len(s)):
+            curr_char = s[i]
+            window[curr_char] += 1
 
-        def findAnagrams(s, p):
-            result = []
-            target = collections.Counter(p)
-            window = collections.Counter(s[:len(p) - 1])
-            for i in range(len(p) - 1, len(s)):
-                curr_char = s[i]
-                window[curr_char] += 1
-
-                if window == target:
-                    result.append(i - len(p) + 1)
-                window[s[i - len(p) + 1]] -= 1
-                if window[s[i - len(p) + 1]] == 0:
-                    del window[s[i - len(p) + 1]]
-            return result
+            if window == target:
+                result.append(i - len(p) + 1)
+            window[s[i - len(p) + 1]] -= 1
+            if window[s[i - len(p) + 1]] == 0:
+                del window[s[i - len(p) + 1]]
+        return result
