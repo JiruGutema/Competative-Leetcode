4b6c6d484a66bd17f5122d72040a5ceb43451ed2
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2024-12-15 15:27:15 +0300

    Time: 89 ms (56.6%), Space: 26.4 MB (17.38%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        minbuy = float('inf')
-        maxsell=0
-        for i in range (0,len(prices)):
-            minbuy=min(minbuy,prices[i])
-            maxsell=max(maxsell,prices[i]-minbuy)
-        return maxsell
-        """The key steps are:
 
-        It initializes two variables: minbuy and maxsell.
-        minbuy keeps track of the minimum stock price seen so far.
-        maxsell keeps track of the maximum profit that can be obtained.
-        It iterates through the prices list.
-        For each price prices[i], it updates minbuy to be the minimum of the current minbuy and prices[i].
-        It then updates maxsell to be the maximum of the current maxsell and the difference between prices[i] and minbuy.
-        Finally, it returns the maximum profit stored in maxsell."""
+        l = 0
+        r = 1
+        profit = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = max(profit, prices[r]-prices[l])
+            else:
+                l = r
+            r += 1
+        return profit
+  
