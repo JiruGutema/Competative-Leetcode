83f8fe34007d08c117dec3dba96012078f46042f
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2024-12-17 23:26:54 +0300

    Time: 239 ms (25.51%), Space: 18.4 MB (5.78%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,39 +1,19 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # k = len(p)
-        # p = sorted(p)
-        # start = 0
-        # end = k
-        # ans = []
+        p_count = Counter(p)
+        s_count = Counter()
+        res = []
+        p_length = len(p)
 
-        # while end < len(s)+1:
-        #     if p == sorted(s[start:end]):
-        #         ans.append(start)
-        #     start += 1
-        #     end += 1
-        # return ans
-
-
-        result = []
-        target = collections.Counter(p)
-        window = collections.Counter(s[:len(p)])
-        if target == window:
-            result.append(0)
-        left = 0
-
-        for i in range(len(p), len(s)):
-
-            curr_char = s[i]
-            window[curr_char] += 1
-
-            char = s[left]
-            window[char] -= 1
-
-            if window[char] == 0:
-                del window[char]
-
-            if window == target:
-                result.append(left+1)
-            left += 1
+        for i in range(len(s)):
+            s_count[s[i]] += 1
+            if i >= p_length:
+                if s_count[s[i - p_length]] == 1:
+                    del s_count[s[i - p_length]]
+                else:
+                    s_count[s[i - p_length]] -= 1
             
-        return result
+            if s_count == p_count:
+                res.append(i - p_length + 1)
+
+        return res
