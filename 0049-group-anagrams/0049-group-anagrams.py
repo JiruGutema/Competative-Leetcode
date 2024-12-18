5438fffa0010ed2345941763d56f1ec65511c8c1
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2024-12-19 01:13:46 +0300

    Time: 15 ms (49.62%), Space: 21.3 MB (35.1%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        sortedToHashMap = {}
-        
+        hashMap = {}
+        ans = []
+
         for i in strs:
-            sortedd = "".join(sorted(i))
-            if sortedd in sortedToHashMap:
-                sortedToHashMap[sortedd].append(i)
+            arranged =str(sorted(i))
+
+            if arranged in hashMap:
+                ans[hashMap[arranged]].append(i)
             else:
-                sortedToHashMap[sortedd] = [i]
-        return list(sortedToHashMap.values())
+                ans.append([i])
+                hashMap[arranged] = len(ans)-1
+        return ans
+                
