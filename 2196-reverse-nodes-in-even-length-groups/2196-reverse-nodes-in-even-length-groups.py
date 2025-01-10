aa0215f1d78a048e6e28f5440e26e13e7e173c19
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-01-10 23:23:36 +0300

    Time: 145 ms (13.86%), Space: 40.8 MB (8.66%) - LeetHub

---
+++ 2196-reverse-nodes-in-even-length-groups/2196-reverse-nodes-in-even-length-groups.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        n, node = 0, head
+        while node: n, node = n+1, node.next
+        
+        k, node = 0, head 
+        while n: 
+            k += 1
+            size = min(k, n)
+            stack = []
+            if not size & 1: 
+                temp = node 
+                for _ in range(size): 
+                    stack.append(temp.val)
+                    temp = temp.next 
+            for _ in range(size): 
+                if stack: node.val = stack.pop()
+                node = node.next 
+            n -= size
+        return head 
