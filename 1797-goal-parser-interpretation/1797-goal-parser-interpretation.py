4248eace125afd503f66e8e322b99ffb95adf398
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-01-31 13:26:28 +0300

    Time: 39 ms (45.15%), Space: 17.6 MB (47.56%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.py
@@ -1,6 +1,21 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        command = command.replace("G", "G")
-        command = command.replace("()", "o")
-        command = command.replace("(al)", 'al')
-        return command
+            # brute force
+        # command = command.replace("G", "G")
+        # command = command.replace("()", "o")
+        # command = command.replace("(al)", 'al')
+        # return command
+            # even better
+        i = 0
+        string = []
+        while i < len(command):
+            if command[i] == "G":
+                string.append("G")
+                i += 1
+            elif command[i] == "(" and command[i+1] == ")":
+                string.append("o")
+                i += 2
+            else:
+                string.append("al")
+                i += 4
+        return "".join(string)
