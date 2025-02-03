d6117f23e628691b79673b8643135b99227f0f49
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-03 16:16:57 +0300

    Time: 3 ms (18.01%), Space: 17.9 MB (28.3%) - LeetHub

---
+++ 2005-check-if-all-the-integers-in-a-range-are-covered/2005-check-if-all-the-integers-in-a-range-are-covered.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isCovered(self, ranges: List[List[int]], left: int, right: int) -> bool:
+        tracker = set()
+
+        for nums in ranges:
+            for num in range(nums[0], nums[1]+1):
+                tracker.add(num)
+        if left not in tracker or right not in tracker:
+            return False
+        for i in range(left, right+1):
+            if i not in tracker:
+                return False
+            
+        return True
