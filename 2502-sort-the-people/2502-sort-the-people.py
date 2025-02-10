dee9b8234a93e8d89abd6e506c116a779271f2a3
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-10 15:35:59 +0300

    Time: 227 ms (10.21%), Space: 18.1 MB (65.23%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,11 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        # bubble sort
+
         # for i in range(len(heights)):
         #     for j in range(len(heights)-1-i):
         #         if heights[j] < heights[j+1]:
         #             names[j], names[j+1] = names[j+1], names[j]
         #             heights[j], heights[j+1] = heights[j+1], heights[j]
         # return names
+
+        # selection sort
+        
         for i in range(len(heights)):
             min_idx = i
             for j in range(i, len(heights)):
