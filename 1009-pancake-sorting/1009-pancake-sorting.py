8261f3df7f92118ee8eb08d6e64d8b1da3650abb
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-12 14:08:00 +0300

    Time: 1 ms (82.95%), Space: 18 MB (19.82%) - LeetHub

--- 1009-pancake-sorting/1009-pancake-sorting.py
+++ 1009-pancake-sorting/1009-pancake-sorting.py
@@ -1,15 +1,17 @@
-from typing import List
-
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
-        
         res = []
         for i in range(len(arr), 1, -1):
             max_index = arr.index(max(arr[:i]))
+
             if max_index != i - 1:
                 if max_index != 0:
+
                     arr[:max_index+1] = arr[:max_index+1][::-1]
                     res.append(max_index + 1)
+
+
                 arr[:i] = arr[:i][::-1]
                 res.append(i)
+                
         return res
