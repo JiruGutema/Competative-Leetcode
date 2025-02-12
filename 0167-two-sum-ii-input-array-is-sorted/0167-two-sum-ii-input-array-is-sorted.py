708817183a45bd5f3b116c9c25aad0461276be43
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-12 11:15:43 +0300

    Time: 7 ms (26.16%), Space: 18.5 MB (56.89%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,13 @@
-from typing import List
-
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        # brute force
-        # n = len(numbers)
-        # for i in range(n):
-        #     for j in range(i, n):
-        #         if numbers[i] + numbers[j] == target:
-        #             return [i+1, j+1]
-        # time complexity O(n^2)
-        # space complexity O(1)
-
-        # efficient approach
-        
         i = 0
         j = len(numbers) -1
-        while j >= i:
-            currentSum = numbers[i] + numbers[j]
-            if currentSum == target:
-                return [i+1, j+1]
-            elif currentSum > target:
+
+        while i < j:
+            current_sum = numbers[i] + numbers[j]
+            if  current_sum > target:
                 j -= 1
-            else:
+            elif current_sum < target:
                 i += 1
-        
+            else:
+                return [i+1,j+1]
