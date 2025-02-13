48bb215afdc667a55167ab31002cc645404bbd4f
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-13 19:06:02 +0300

    Time: 0 ms (100%), Space: 25.7 MB (44.68%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -5,3 +5,28 @@ class Solution:
         nums.reverse()
         nums[:k] = reversed(nums[:k])
         nums[k:] = reversed(nums[k:])
+
+
+        # if k == 0 or len(nums) <= 1:
+        #     return nums
+        # curr = nums[0]
+        # count = 0
+        # idx = 0
+        # start = 0
+
+        # while count < len(nums):
+        #     nxt_idx = (idx + k) % len(nums)
+        #     temp = nums[nxt_idx]
+        #     nums[nxt_idx] = curr
+        #     curr = temp
+        #     count += 1
+        #     idx = nxt_idx
+            
+        #     if idx == start:
+        #         start += 1
+        #         idx = start
+        #         curr = nums[idx]
+
+        # return (nums)
+
+        
