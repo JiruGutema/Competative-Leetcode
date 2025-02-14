e2424da5eb3e5e3e05482b23f6fdec634da098ba
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-14 11:19:33 +0300

    Time: 1481 ms (5.01%), Space: 18.5 MB (16.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,26 +1,32 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if t == "":
+        t = Counter(t)
+
+        n = len(s)
+        ans = [0, len(s)]
+        Window = Counter()
+        left = 0
+        for right in range(n):
+            Window[s[right]] += 1
+            
+            while Window >= t:
+    
+                if (right-left) <= (ans[1] - ans[0]):
+                    ans[0] = left
+                    ans[1] = right
+                if s[left] in Window:
+                    Window[s[left]] -= 1
+                    if Window[s[left]] == 0:
+                        del Window[s[left]]
+                left += 1
+
+        if ans[1] == n:
             return ""
-        countT,window={},{}
-        for c in t:
-            countT[c]=1+countT.get(c,0)
-        have,need=0,len(countT)
-        res,resLen=[-1,-1],float("infinity")
-        l=0
-        for r in range(len(s)):
-            c=s[r]
-            window[c]=1+window.get(c,0)
-            if(c in countT and window[c] == countT[c]):
-                have+=1
-            while have == need:
-                if (r - l + 1) < resLen:
-                    res = [l,r]
-                    resLen = r - l + 1
-                window[s[l]] -= 1
-                if(s[l] in countT and window[s[l]] < countT[s[l]]):
-                    have-=1
-                l += 1
-        l,r = res
-        return s[l:r+1] if resLen != float("infinity") else ""
+        return(s[ans[0]:ans[1]+1])
+        
+        
+            
+
+
+
+        
