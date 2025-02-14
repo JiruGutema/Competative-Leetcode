5e3ad65f9074744ee82bea0d706c9e01706fa4f2
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-14 15:46:21 +0300

    Time: 11 ms (9.73%), Space: 18 MB (14.25%) - LeetHub

---
+++ 0768-partition-labels/0768-partition-labels.py
@@ -0,0 +1,32 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        n = len(s)
+        i = 0
+        j = 0
+        res = []
+        seen = defaultdict(int)
+        check = set()
+
+        for k in range(n):
+            seen[s[k]] = seen.get(s[k], 0) + 1
+
+        while i < n and i <= j:
+
+            seen[s[j]] = seen[s[j]] - 1
+            check.add(s[j])
+
+            if seen[s[j]] == 0:
+                check.remove(s[j])
+            
+            if len(check) == 0:
+                res.append(j-i+1)
+                j += 1
+                i = j
+            else:
+                j += 1
+
+        return res
+
+
+        
+        
