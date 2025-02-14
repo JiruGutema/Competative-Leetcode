c4e8ebbd3b4e6a9f26e736ce1b8325f084f26bc8
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-14 17:05:11 +0300

    Time: 43 ms (87.22%), Space: 17.8 MB (96.96%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+
+        count = 0
+        zero_count = 0  
+        for char in s:
+            if char == '0':
+                count += zero_count  
+            else:
+                zero_count += 1
+        
+        return count
+
+
+                
