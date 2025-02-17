17c17d716506148c0038333a10f976bb6dea53f0
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-17 19:22:27 +0300

    Time: 3 ms (13.92%), Space: 17.8 MB (39.33%) - LeetHub

--- 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.py
@@ -2,22 +2,19 @@ class Solution:
     def equalFrequency(self, word: str) -> bool:
         count = Counter(word)
         val = list(count.values())
+
         
-        if word == "aachqpawxanqdkcdycjkewbmiavafhrzzfxrolfbqvywqoiqzdgopjngpgnwiguitudbawbyxputjafdkfgrojdokj":
-            return False
-        if "zz" in word and word != "aazz" :
-            return True
-        if len(set(count.values())) == 1 and 1 not in set(count.values()):
-            return False
         for i in list(count.keys()):
             count[i] -= 1
             print("before",count )
             if count[i] == 0:
                 del count[i]
+                print("middle",count )
             if len(set(count.values())) == 1:
                 return True
             count[i] += 1
             print("after",count )
+            print()
 
         return False
 
