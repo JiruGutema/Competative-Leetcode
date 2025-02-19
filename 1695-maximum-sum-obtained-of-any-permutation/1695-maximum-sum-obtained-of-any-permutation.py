9fb07180a663b302f862e07636ca83025629e305
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-19 11:19:24 +0300

    Time: 89 ms (61.66%), Space: 45.3 MB (16.58%) - LeetHub

--- 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.py
+++ 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.py
@@ -1,24 +1,19 @@
-from typing import List
-
 class Solution:
     def maxSumRangeQuery(self, nums: List[int], requests: List[List[int]]) -> int:
-        MOD = int(1e9 + 7)
-        n = len(nums)
-        sums = [0] * (n + 1)  
-        
-        for start, end in requests:
-            sums[start] += 1
-            if end + 1 < n:
-                sums[end + 1] -= 1
-        
-        for i in range(1, n):
-            sums[i] += sums[i - 1]
-        
-        sums.pop()
-
         nums.sort(reverse=True)
-        sums.sort(reverse=True)
-
-        ans = sum(nums[i] * sums[i] for i in range(n)) % MOD
-
-        return ans
+        n = len(nums)
+        aums = [0] * n
+        for start, end in requests:
+            aums[start] += 1
+            if end + 1 < n:
+                aums[end + 1] -= 1
+        for i in range(1, n):
+            aums[i] += aums[i - 1]
+        aums.sort(reverse=True)
+        
+        ans = 0
+        mod = int(1e9 + 7)
+        for i in range(len(nums)):
+            ans += nums[i] * aums[i]
+        
+        return int(ans % mod)
