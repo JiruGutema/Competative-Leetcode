9ff269d8a82ea0a5f080cfec50b44f73061e817c
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-02-26 19:34:19 +0300

    Time: 105 ms (49.38%), Space: 23.5 MB (50.61%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,21 +1,27 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
-        max_length =0
-        hash={}
-        count=0
-        for i in range(len(nums)):
-            current=nums[i]
-            if current==0:
-                count-=1 # decrement our count if our current element is 0
-            else:
-                # increment our count if current element is 1
-             count+=1
+        n = len(nums)
+        idx_tracker = {}
+        count = 0
+        max_len = 0
 
-            if count==0:
-                # if count is 0, we have a new subarray with length+1
-                max_length=i+1
-            if count in hash:
-                max_length=max(max_length,i-hash[count]) 
+        for i in range(len(nums)):
+            current = nums[i]
+            if current == 0:
+                count -= 1
             else:
-                hash[count]=i
-        return max_length
+                count += 1
+
+            if count == 0:
+                max_len = i+1
+
+            if count in idx_tracker:
+                max_len = max(max_len, i-idx_tracker[count])
+            else:
+                idx_tracker[count] = i
+        return max_len
+
+
+        
+        
+
