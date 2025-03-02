a445ab7cef6e4100956861bbc5befd9b83267fbc
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-03-02 22:35:37 +0300

    Time: 0 ms (100%), Space: 18 MB (23.71%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,38 @@
+
+
+class MyStack:
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        if self.empty():
+            raise IndexError("Pop from an empty stack")
+        
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        top = self.q1.popleft()
+        
+        # Swap the roles of q1 and q2
+        self.q1, self.q2 = self.q2, self.q1
+        return top
+
+    def top(self) -> int:
+        if self.empty():
+            raise IndexError("Top from an empty stack")
+        
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        top = self.q1[0]
+        self.q2.append(self.q1.popleft())
+        
+        self.q1, self.q2 = self.q2, self.q1
+        return top
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
