7c095c3ef04e7aaa484c4e1372bceb53a24710ca
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-03-28 18:37:07 +0300

    Time: 0 ms (100%), Space: 18 MB (19.83%) - LeetHub

---
+++ 1873-longest-nice-substring/1873-longest-nice-substring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        if not s:
+            return s
+
+        def check(s, char):
+            if char.islower():
+                return char.upper() in s
+            else:
+                return char.lower() in s
+
+        def recursion(s):
+            for i in range(len(s)):
+                if not check(s, s[i]):
+                    s1 = recursion(s[:i])
+                    s2 = recursion(s[i+1:])
+                    return s2 if len(s2) > len(s1) else s1 
+            return s 
+
+        return recursion(s)
