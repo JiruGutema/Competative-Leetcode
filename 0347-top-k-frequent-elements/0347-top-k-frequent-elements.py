daf6662b1cc68822f219d627e48d856cab8be041
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-04-01 20:09:13 +0300

    Time: 4 ms (70.19%), Space: 21.3 MB (61.97%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,8 @@
-from typing import List
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hashmap = {}
+        # brute force
+
+        nums = Counter(nums)
+        nums = sorted(nums.items(), key=lambda key: key[1], reverse=True)
+        return [num[0] for num in nums[:k]]
         
-        # Count the frequency of each number
-        for i in nums:
-            if i in hashmap:
-                hashmap[i] += 1
-            else:
-                hashmap[i] = 1
-        
-        # Sort the hashmap by frequency and get the top k elements
-        # Create a list of tuples (key, value) and sort by value in descending order
-        sorted_items = sorted(hashmap.items(), key=lambda item: item[1], reverse=True)
-        
-        # Extract the top k elements
-        ans = [item[0] for item in sorted_items[:k]]
-        
-        return ans
