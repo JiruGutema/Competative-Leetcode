159f44186ec0c07411f4eb88c5b72b4d413a6611
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-04-03 19:37:31 +0300

    Time: 4 ms (6.8%), Space: 17.9 MB (33.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,9 +1,33 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         
-        for i in range(len(nums)):
-            for j in range(1, len(nums)-i):
-                if nums[j] < nums[j-1]:
-                    nums[j], nums[j-1] = nums[j-1], nums[j]
-        return nums
+        def quick(nums, left, right):
+            if left >= right:  
+                return
+            pivot = partition(nums, left, right)
 
+            quick(nums, left, pivot - 1)
+            quick(nums, pivot + 1, right)
+
+        def partition(nums, left, right):
+            random_index = randint(left, right)
+            nums[left], nums[random_index] = nums[random_index], nums[left]
+            pivot = left
+
+            holder = left + 1
+
+            for seeker in range(holder, right + 1):
+                if nums[seeker] <= nums[pivot]:
+                    nums[seeker], nums[holder] = nums[holder], nums[seeker]
+                    holder += 1
+
+            nums[pivot], nums[holder - 1] = nums[holder - 1], nums[pivot]
+
+            return holder - 1
+
+     
+        quick(nums, 0, len(nums) - 1)
+        return nums
