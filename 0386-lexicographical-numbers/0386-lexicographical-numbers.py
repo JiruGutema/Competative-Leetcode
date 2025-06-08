9cde7f4f89b87117b8a3f355607e56d84fab9bbf
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-06-08 18:38:14 +0300

    Time: 47 ms (27.82%), Space: 23.3 MB (20.38%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -2,5 +2,11 @@ class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
         space = []
         for i in range(1,n+1):
-            space.append(str(i))
-        return [int(i) for i in sorted(space)]
+            heapq.heappush(space, str(i))
+        ans = []
+
+        while space:
+            ans.append(int(heapq.heappop(space)))
+        return ans
+
+
