70a409889c0f0146efcd3625bbc41f7cd1320c23
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-06-29 22:56:11 +0300

    Time: 161 ms (64.87%), Space: 28.3 MB (40.29%) - LeetHub

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/1621-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,18 +1,16 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
-        res, mod = 0, 1000000007
-        l, r = 0, len(nums) - 1
-        pre = [1]
-        for i in range(1, len(nums) + 1):
-            pre.append((pre[-1] << 1) % mod)
-                
+        MOD = 10 ** 9 + 7
+        nums = [num for num in nums if num < target]
         nums.sort()
-        
-        while l <= r:
-            if nums[l] + nums[r] > target:
-                r -= 1
+        n = len(nums)
+        left, right = 0, n - 1
+        res = 0
+        while left <= right:
+            if nums[left] + nums[right] > target:
+                right -= 1
             else:
-                res = (res + pre[r - l]) % mod
-                l += 1
-
+                size = right - left  # ignore the number at left
+                res = (res + pow(2, size, MOD)) % MOD   # nC0 + nC1 + nC2 + ... + nCn
+                left += 1
         return res
