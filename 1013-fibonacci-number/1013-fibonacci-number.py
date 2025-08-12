7b3febbf5582c2106bade2d4781bbe7d9964ef56
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-08-12 23:07:18 +0300

    Time: 40 ms (60.75%), Space: 17.6 MB (89.35%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,8 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
         # top down
-
         # memo = {0:0, 1:1}
+
         # def dp(n):
         #     if n in memo:
         #         return memo[n]
@@ -10,14 +10,22 @@ class Solution:
         #         memo[n] = dp(n-1) + dp(n-2)
         #     return memo[n]
         # return dp(n)
+        
+        # bottom up
 
-        #  bottom up
         if n <= 1:
             return n
+            
         memo = [0]*(n+1)
         memo[1] = 1
         base = 2
+
         while base < n+1:
             memo[base] = memo[base-1] + memo[base-2]
             base += 1
-        return memo[base-1]
+        return memo[base - 1]
+
+
+
+
+
