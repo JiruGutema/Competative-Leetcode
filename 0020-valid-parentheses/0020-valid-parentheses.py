37c413a09dede0666912cf85af08d90e932d7fc3
JiruGutema <166136751+JiruGutema@users.noreply.github.com>
2025-08-19 20:01:28 +0300

    Time: 0 ms (100%), Space: 17.7 MB (80.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,14 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        pairs = {
-            '(': ')','{': '}','[': ']'}
+        mapped = {"{":"}","[":']',"(":")"}
+        n = len(s)
 
-        for i in s:
-            if i in pairs:
-                stack.append(i)
-            else: 
-                if not stack or pairs[stack.pop()] != i:
+        for i in range(n):
+            if s[i] in mapped.keys():
+                stack.append(s[i])
+            
+            else:
+                if not stack:
                     return False
+                top = stack.pop()
 
+                if mapped[top] != s[i]:
+                    return False
         return not stack
