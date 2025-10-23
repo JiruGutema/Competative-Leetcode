979847e52dbb1c9666b1d2d6fb8272644fef5a32
Jiru Gutema <166136751+JiruGutema@users.noreply.github.com>
2025-10-23 11:03:53 +0300

    Time: 228 ms (23.47%), Space: 57.8 MB (64.34%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,32 +1,35 @@
 class RandomizedSet:
+
     def __init__(self):
-        self.stack = []
-        self.dict = {}
-        
+        self.random_set = set()
+        self.array = []
 
     def insert(self, val: int) -> bool:
-        if val in self.dict:
+        if(val not in self.random_set):
+            self.random_set.add(val)
+            self.array.append(val)
+            return True
+        else:
             return False
-
-        self.dict[val] = len(self.stack)
-        self.stack.append(val)
-        return True
         
     def remove(self, val: int) -> bool:
-        if val not in self.dict:
-            return False        
-        last = self.stack[-1]
-        ind = self.dict[val] 
+        if(val in self.random_set):
+            self.random_set.remove(val)
+            self.array.remove(val)
+            return True
+        else:
+            return False
         
-        self.dict[last] = ind
-        self.stack[ind] = last
-        
-        self.stack.pop()
-        
-        self.dict.pop(val)
-        
-        
-        return True        
 
     def getRandom(self) -> int:
-        return random.choice(self.stack)
+        idx = random.randint(0, len(self.random_set) - 1)
+        picked = self.array[idx]
+        return picked
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
